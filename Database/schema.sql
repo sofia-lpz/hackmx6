95613e1901529d2addb06aab6ec22be25b9873c5
sofia-lpz <a01028251@tec.mx>
2024-10-25 19:11:53 -0600

    final schema

--- Database/schema.sql
+++ Database/schema.sql
@@ -2,13 +2,33 @@ DROP DATABASE IF EXISTS abarrotes;
 CREATE DATABASE IF NOT EXISTS abarrotes;
 USE abarrotes;
 
+
+CREATE TABLE proveedores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255),
+    telefono VARCHAR(255),
+    periodo enum('diario', 'semanal', 'mensual')
+    ultima_fecha DATE
+);
+
 CREATE TABLE productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre_producto VARCHAR(255) NOT NULL,
     cantidad INT,
     gramos_por_unidad FLOAT,
     cantidad_gramos FLOAT,
-    cantidad_kilogramos FLOAT
+    cantidad_kilogramos FLOAT,
+    precio FLOAT
+    proveedor_id INT,
+    FOREIGN KEY (proveedor_id) REFERENCES proveedores(id)
+);
+
+CREATE TABLE ventas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT,
+    cantidad INT,
+    fecha DATE,
+    FOREIGN KEY (id_producto) REFERENCES productos(id)
 );
 
 DELIMITER //
